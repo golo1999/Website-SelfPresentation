ddf541bdb20cac4caf66e470c46316358583c5d6
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-10-08 00:32:49 +0300

    Refactoring the code with Prettier

--- assets/formValidation.js
+++ assets/formValidation.js
@@ -5,13 +5,16 @@ const subjectInput = document.querySelectorAll("input[type=text]")[1];
 const messageInput = document.querySelector("textarea");
 const sendEmail = document.querySelector("button");
 
-sendEmail.onclick = () =>
-{
-    if(nameInput.value !== '' && emailInput.value !== '' && subjectInput.value !== '' && messageInput.value !== '')
-        form.submit();
-    else
-    {
-        alert("Please complete the form first");
-        return false;
-    }
+sendEmail.onclick = () => {
+  if (
+    nameInput.value !== "" &&
+    emailInput.value !== "" &&
+    subjectInput.value !== "" &&
+    messageInput.value !== ""
+  ) {
+    form.submit();
+  } else {
+    alert("Please complete the form first");
+    return false;
+  }
 };
--- server.js
+++ server.js
@@ -6,70 +6,111 @@ const port = 3000 || process.env.PORT;
 const path = require("path").dirname(require.main.filename);
 const nodeMailer = require("nodemailer");
 const mailGun = require("nodemailer-mailgun-transport");
-const auth = {auth:{apiKey: process.env.API_KEY, domain: process.env.DOMAIN}};
+const auth = {
+  auth: { apiKey: process.env.API_KEY, domain: process.env.DOMAIN },
+};
 const transporter = nodeMailer.createTransport(mailGun(auth));
 const mongo = require("mongodb");
-const mongoClient=mongo.MongoClient;
-const assert=require("assert");
-const dbURL="mongodb://localhost:127.0.0.1:27017/self_presentation";
+const mongoClient = mongo.MongoClient;
+const assert = require("assert");
+const dbURL = "mongodb://localhost:127.0.0.1:27017/self_presentation";
 
-app.use("/assets", express.static(__dirname+'/assets'));
-app.use(express.urlencoded({extended: true}));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-addToDatabase = (name, email, subject, message, postResponse) => // function for saving the data into the database (I AM USING A MONGODB NOSQL DATABASE)
-{
-    mongoClient.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => // creating a connection to the database
-    {
-        const dbObject=db.db("mailDB"); // database name
+// function for saving the data into the database
+const addToDatabase = (name, email, subject, message, postResponse) => {
+  // creating a connection to the database
+  mongoClient.connect(
+    dbURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, db) => {
+      // database name
+      const dbObject = db.db("mailDB");
+      // redirecting to the fail route if there was an error
+      if (err) {
+        postResponse.redirect("/fail");
+      }
 
-        if(err) // if there was an error, we'll be redirected to the fail route
-            postResponse.redirect("/fail");
-        dbObject.collection("self_presentation").insertOne({email: email, name: name, subject: subject, message: message}, (err1, result) => // inserting the data
-        {
+      // inserting the data
+      dbObject
+        .collection("self_presentation")
+        .insertOne(
+          { email: email, name: name, subject: subject, message: message },
+          (err1, result) => {
             assert.strictEqual(null, err1);
-            db.close(); // closing the connection to the database
-        });
+            // closing the connection to the database
+            db.close();
+          }
+        );
 
-        postResponse.redirect("/success"); // if there wasn't any error, we'll be redirected to the success route
-    });  
-}
+      // redirecting to the success route if there wasn't any error
+      postResponse.redirect("/success");
+    }
+  );
+};
 
-app.get("/", (req, res) => // root route
-{
-    res.redirect("/home");
+// root route
+app.get("/", (req, res) => {
+  res.redirect("/home");
 });
 
-app.get("/home", (req, res) => // home route
-{
-    res.sendFile(path + '/index.html');
+// home route
+app.get("/home", (req, res) => {
+  res.sendFile(path + "/index.html");
 });
 
-app.get("/fail", (req, res) => // fail route
-{
-    res.render("response", {message: "Oops! Something didn't work as expected", title: "Fail"});
+// fail route
+app.get("/fail", (req, res) => {
+  res.render("response", {
+    message: "Oops! Something didn't work as expected",
+    title: "Fail",
+  });
 });
 
-app.get("/success", (req, res) => // success route
-{
-    res.render("response", {message: "Yey! Everything worked properly", title: "Success"});
+// success route
+app.get("/success", (req, res) => {
+  res.render("response", {
+    message: "Yey! Everything worked properly",
+    title: "Success",
+  });
 });
 
-app.post("/send-form", (req, res) => // route where we'll be redirected if the form was submitted -> route for sending the mail
-{
-    const mailOptions = {from: req.body.email, to: process.env.EMAIL, subject: req.body.subject, text: req.body.message};
+// route where we'll be redirected if the form was submitted -> route for sending the mail
+app.post("/send-form", (req, res) => {
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: req.body.subject,
+    text: req.body.message,
+  };
 
-    transporter.sendMail(mailOptions, (err) =>
-    {
-        if(err) // if there was an error, we'll be redirected to the fail route
-            res.redirect("/fail");
-        else addToDatabase(req.body.name, req.body.email, req.body.subject, req.body.message, res); // if there wasn't any error, we'll be adding the data into the database
-    });
+  transporter.sendMail(mailOptions, (err) => {
+    // redirecting to the fail route if there was an error
+    if (err) {
+      res.redirect("/fail");
+    }
+    // adding the data into the database if there aren't any errors
+    else {
+      addToDatabase(
+        req.body.name,
+        req.body.email,
+        req.body.subject,
+        req.body.message,
+        res
+      );
+    }
+  });
 });
 
-app.get("*", (req, res) => // if the route doesn't exit
-{
-    res.render("response", {message: "Oops! The requested page doesn't exist", title: "Not found"});
+// if the route doesn't exit
+app.get("*", (req, res) => {
+  res.render("response", {
+    message: "Oops! The requested page doesn't exist",
+    title: "Not found",
+  });
 });
 
-app.listen(port); // setting up the server
+// setting up the server on the given port
+app.listen(port);
